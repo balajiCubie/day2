b37d3590ced94905aeee004be678a58e73ad7c65
Balaji K <balajibala76394@gmail.com>
2023-04-14 06:43:35 +0530

    commit2

--- notes/api/views.py
+++ notes/api/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+# from  django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET'])
+def  getRoutes(request):
+        data=[
+            {   
+                "endpoint": "/notes",
+                "method": "GET",
+                "body": None,
+                "description": "Returns a list of notes"
+            },
+            {
+                "endpoint": "/notes/id",
+                "method": "GET",
+                "body": None,
+                "description": "Returns a single note"
+            },
+            {
+                "endpoint": "/notes/create",
+                "method": "POST",
+                "body": {"title": "String", "content": "String", "category": "String"},
+                "description": "Create a new note"
+            },
+        ]
+        return  Response(data)
+
